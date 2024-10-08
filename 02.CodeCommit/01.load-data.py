8f9623388513309c99615a760859eaf32967e94b
mujahed85 <mujahed.mca@gmail.com>
2024-10-08 08:10:16 +0000

    Uploading to S3 Done

--- 02.CodeCommit/01.load-data.py
+++ 02.CodeCommit/01.load-data.py
@@ -1,5 +1,6 @@
 # pip install scikit-learn --break-system-packages --no-warn-script-location
 # pip install pandas --break-system-packages --no-warn-script-location
+# pip install boto3 --break-system-packages --no-warn-script-location
 # python3 01.load-data.py
 ##########################Loading Data to DataFrame ###################
 from sklearn.datasets import load_iris
@@ -25,12 +26,12 @@ train_data.to_csv('train.csv', index=False)
 test_data.to_csv('test.csv', index=False)
 
 ####################Uploading to S3 Bucket######################################################
-# import boto3
+import boto3
 
-# # Initialize S3 client
-# s3 = boto3.client('s3')
+# Initialize S3 client
+s3 = boto3.client('s3')
 
-# # Upload data to S3
-# bucket_name = 'bkt-syed-2024-10-08-06-15-56z'
-# s3.upload_file('train.csv', bucket_name, 'data/train.csv')
-# s3.upload_file('test.csv', bucket_name, 'data/test.csv')
+# Upload data to S3
+bucket_name = 'bkt-syed-2024-10-08-06-15-56z'
+s3.upload_file('train.csv', bucket_name, 'data/train.csv')
+s3.upload_file('test.csv', bucket_name, 'data/test.csv')
