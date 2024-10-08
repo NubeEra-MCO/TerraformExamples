1c7a8b70599dee1066a0664b89a4185281001f47
mujahed85 <mujahed.mca@gmail.com>
2024-10-08 08:10:57 +0000

    Command to Verify

--- 02.CodeCommit/01.load-data.py
+++ 02.CodeCommit/01.load-data.py
@@ -35,3 +35,5 @@ s3 = boto3.client('s3')
 bucket_name = 'bkt-syed-2024-10-08-06-15-56z'
 s3.upload_file('train.csv', bucket_name, 'data/train.csv')
 s3.upload_file('test.csv', bucket_name, 'data/test.csv')
+
+#  aws s3 ls s3://bkt-syed-2024-10-08-06-15-56z/data/
